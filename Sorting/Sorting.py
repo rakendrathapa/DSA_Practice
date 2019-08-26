728455e012fa16a8bc6c3d1372e3a71c0b0e5ffa
Rakendra Thapa <rakendra.thapa@infineon.com>
2019-08-26 17:29:44 +0530

    Sorting: Quicksort

--- Sorting/Sorting.py
+++ Sorting/Sorting.py
@@ -1,5 +1,5 @@
 class Sorting:
-    def mergesort(self, arr):
+    def mergesort(self, arr):       # making a copied list
         if len(arr) is None or len(arr) == 1:
             return
 
@@ -28,3 +28,47 @@ class Sorting:
             arr[k] = R[j]
             j += 1
             k += 1
+        return arr
+
+    def __swaparrpos(self, arr, pos1, pos2):
+        arr[pos1], arr[pos2] = arr[pos2], arr[pos1]
+        return arr
+
+
+    def __quicksortutil(self, arr, posL, posR):
+        if posR <= posL:
+            return
+
+        # print('Arr:{}'.format(arr))
+        pivotpos = posL
+        leftpos = posL + 1
+        rightpos = posR
+
+        # print('pivotpos:{} leftpos:{} rightpos:{}'.format(pivotpos, leftpos, rightpos))
+        # print('arr[pivotpos]:{} arr[leftpos]:{} arr[rightpos]:{}'.format(arr[pivotpos], arr[leftpos], arr[rightpos]))
+
+        while leftpos < rightpos:
+            if arr[rightpos] > arr[pivotpos]:
+                rightpos -= 1
+                continue
+            if arr[leftpos] <= arr[pivotpos]:
+                leftpos += 1
+                continue
+
+            self.__swaparrpos(arr, leftpos, rightpos)
+
+        if arr[pivotpos] > arr[leftpos]:
+            self.__swaparrpos(arr, leftpos, pivotpos)
+            pivotpos = leftpos
+
+        self.__quicksortutil(arr, posL, pivotpos)
+        self.__quicksortutil(arr, pivotpos+1, posR)
+
+
+    def quicksort(self, arr):
+        print('Array:{}'.format(arr))
+        if len(arr) == 0 or len(arr) == 1:
+            return arr
+
+        self.__quicksortutil(arr, 0, len(arr)-1)
+        return arr
--- Sorting_Testing.py
+++ Sorting_Testing.py
@@ -1,11 +1,28 @@
 from Sorting import Sorting
 
 def testmain():
-    arr = [38, 27, 43, 3, 9, 82, 10, 12]
-    arr2 = [12, 11, 13, 5, 6, 7]
+    mergesorttestcases = list()
+    mergesorttestcases.append([38, 27, 43, 3, 9, 82, 10, 12])
+    mergesorttestcases.append([12, 11, 13, 5, 6, 7])
+    mergesorttestcases.append([1, 5, 7, 9, 4, 2, 3, 6, 8])
+
     sort = Sorting.Sorting()
-    sort.mergesort(arr2)
-    print(arr2)
+    print('sorting: mergesort')
+    for case in mergesorttestcases:
+        # print('Array:{}'.format(case))
+        sort.mergesort(case)
+        # print('Sorted:{}'.format(case))
+
+    quicksorttestcases = list()
+    quicksorttestcases.append([0])
+    quicksorttestcases.append([38, 27, 43, 3, 9, 82, 10, 12])
+    quicksorttestcases.append([12, 11, 13, 5, 6, 7])
+    quicksorttestcases.append([1, 5, 7, 9, 4, 2, 3, 6, 8])
+    print('sorting: quicksort')
+    for case in quicksorttestcases:
+        # print('Array:{}'.format(case))
+        ans = sort.quicksort(case)
+        print('Sorted:{}'.format(ans))
 
 
 if __name__ == '__main__':
