8f059a31ce6550b19755ad13015d45cc8ff0b57c
Rakendra Thapa <rakendra.thapa@infineon.com>
2019-09-03 16:58:40 +0530

    Graph - Undirected Graph - BFS and DFS

--- Graph/Graph.py
+++ Graph/Graph.py
@@ -1,30 +1,48 @@
-from collections import defaultdict
+from collections import defaultdict, deque
+
 
 class Graph:
-    def __init__(self):
-        self.graph = defaultdict(list)
+    def __init__(self, num_vertices):
+        self.edges = defaultdict(list)
+        self.num_of_vertices = num_vertices
 
-    def addEdge(self,u,v):
-        self.graph[u].append(v)
+    def addEdge(self, u, v):
+        raise NotImplementedError('To be implemented in the subclass')
 
-    def BFS(self, s):
-        if s is None:
+
+class UDGraph(Graph):
+    def __init__(self, n):
+        super().__init__(n)
+
+    def addEdge(self, u, v):
+        self.edges[u].append(v)
+        self.edges[v].append(u)
+
+    def __DFSUtil(self, visited, u):
+        if u is None:
             return
+        visited[u] = True
+        print('{', u, '} --> ', end='')
+        for v in self.edges[u]:
+            if visited[v] is False:
+                self.__DFSUtil(visited, v)
 
-        visited = [False] * (len(self.graph))
-        queue=[]
+    def DFS(self, vertex):
+        visited = [False] * self.num_of_vertices
+        self.__DFSUtil(visited, vertex)
 
-        queue.append(s)
-        visited[s]=True
-
-        while queue:
-            s = queue.pop(0)
-            print(s, end=" ")
-
-            # Get all adjacent vertices of dequeued vertex s.
-            # If adjacent has not been visited, then mark it as
-            # visited and enqueue it.
-            for node in self.graph[s]:
-                if visited[node] == False:
-                    visited[node] = True
-                    queue.append(node)
+    def BFS(self, vertex):
+        if vertex is None:
+            return
+        visited = [False] * self.num_of_vertices
+        queue = deque()
+        queue.appendleft(vertex)
+        visited[vertex] = True
+        while len(queue):
+            u = queue.pop()
+            # print('{', u, '} --> ', end='')
+            for v in self.edges[u]:
+                if visited[v] is False:
+                    queue.appendleft(v)
+                    visited[v] = True
+            yield u
